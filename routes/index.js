4b4cca5dc0f5325e3fb0899f4802199504727109
Basse <babacar.basse@esp,sn>
2020-11-24 22:59:57 +0000

    new update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
 //   log.debug("This is in the index module");
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express update' });
 });
 
 module.exports = router;
